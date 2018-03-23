b8c2292178dcd162b7f45049ed4bc2ad18eb9f9a
Fozat Kardouh <fozatkardouh@gmail.com>
2018-03-23 11:16:16 +0100

    added empty project

---
+++ src/main/java/at/refugeescode/mp6_the_three_cup_shuffle/Mp6TheThreeCupShuffleApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp6_the_three_cup_shuffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp6TheThreeCupShuffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp6TheThreeCupShuffleApplication.class, args);
+	}
+}
